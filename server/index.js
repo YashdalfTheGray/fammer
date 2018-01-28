7cec0817c65487a641972dff1514e1eae45e361c
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2018-01-27 16:25:33 -0800

    added more of the server stuff to the project

---
+++ server/index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import morgan from 'morgan';
+import bodyParser from 'body-parser';
+import chalk from 'chalk';
+
+// import { wrap } from './utils';
+
+const port = process.env.PORT || process.argv[2] || 8080;
+const app = express();
+
+app.use(bodyParser.json());
+
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+else {
+    app.use(morgan('combined'));
+}
+
+app.listen(port, () => {
+    console.log(`Server now running on port ${chalk.green(port)}`);
+});
