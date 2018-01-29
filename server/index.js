4b55260080952bfc6de68222751cfb95b35cea03
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2018-01-28 19:52:09 -0800

    serve up a public directory where we will build our app to

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+import { resolve } from 'path';
 import express from 'express';
 import morgan from 'morgan';
 import bodyParser from 'body-parser';
@@ -9,6 +10,7 @@ const port = process.env.PORT || process.argv[2] || 8080;
 const app = express();
 
 app.use(bodyParser.json());
+app.use(express.static(resolve(process.cwd(), 'public')));
 
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
