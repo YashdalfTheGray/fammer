21a0b5784ddbfc690e06769e48c9b33e481f48ad
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2018-01-30 18:43:09 -0800

    added some dummy vue code

---
+++ src/Fammer.vue
@@ -0,0 +1,25 @@
+<template>
+    <div id="fammer">
+        {{ appName }}
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'fammer',
+    data: () => ({
+        appName: 'Fammer'
+    })
+};
+</script>
+
+<style>
+#fammer {
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    flex: 1 1 auto;
+}
+.md-app {
+    flex: 1 1 auto;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,14 @@
+import Vue from 'vue';
+import VueMaterial from 'vue-material';
+import 'vue-material/dist/vue-material.min.css';
+import 'vue-material/dist/theme/default.css';
+
+import App from './Fammer.vue';
+
+Vue.use(VueMaterial);
+
+// eslint-disable-next-line no-new
+new Vue({
+    el: '#app',
+    render: h => h(App)
+});
