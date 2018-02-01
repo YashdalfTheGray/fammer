b783313f289d7beb08260d5454c7c981feccaea0
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2018-01-31 18:17:03 -0800

    filled out some more of the template and set some styles on body

--- src/Fammer.vue
+++ src/Fammer.vue
@@ -1,7 +1,21 @@
 <template>
-    <div id="fammer">
-        {{ appName }}
-    </div>
+    <md-app id="fammer" md-waterfall md-mode="fixed" class="fammer-app-shell">
+        <md-app-toolbar class="md-primary">
+            <h3 class="md-title" style="flex: 1">{{ appName }}</h3>
+            <md-button>Log In</md-button>
+        </md-app-toolbar>
+        <md-app-content class="md-scrollbar">
+            <p>Test Test</p>
+            <p>Test Test</p>
+            <p>Test Test</p>
+            <p>Test Test</p>
+            <p>Test Test</p>
+            <p>Test Test</p>
+            <p>Test Test</p>
+            <p>Test Test</p>
+            <p>Test Test</p>
+        </md-app-content>
+    </md-app>
 </template>
 
 <script>
@@ -13,7 +27,7 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
 #fammer {
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
