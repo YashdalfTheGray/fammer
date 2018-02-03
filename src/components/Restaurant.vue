54e7b31f3c237afb89b2735ead3add1e57cfa757
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2018-02-03 18:38:55 -0500

    Added a couple of components to the mix

---
+++ src/components/Restaurant.vue
@@ -0,0 +1,26 @@
+<template>
+    <md-card id="restaurant">
+        <md-card-header>
+            <div class="md-itle">{{ title }}</div>
+        </md-card-header>
+        <md-card-content>This is where information about the card will go.</md-card-content>
+        <md-card-action>
+            <md-button>Map</md-button>
+        </md-card-action>
+    </md-card>
+</template>
+
+<script>
+export default {
+    name: 'restaurant',
+    data() {
+        return {
+            title: `Fat's Chicken and Waffles`
+        };
+    }
+};
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/RestaurantGrid.vue
@@ -0,0 +1,29 @@
+<template>
+    <md-content id="restaurant-grid" class="restaurant-list">
+        <restaurant></restaurant>
+        <restaurant></restaurant>
+        <restaurant></restaurant>
+        <restaurant></restaurant>
+        <restaurant></restaurant>
+    </md-content>
+</template>
+
+<script>
+import Restaurant from './Restaurant.vue';
+
+export default {
+    name: 'restaurant-grid',
+    data() {
+        return {};
+    },
+    components: {
+        Restaurant
+    }
+};
+</script>
+
+<style scoped>
+    .restaurant-list > * + * {
+        margin-top: 8px;
+    }
+</style>
