19f1d7df78ac3c7568a739fa5093d394495f1fb9
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2018-02-02 20:01:12 -0500

    added vuefire bindings to Vue

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
 import Vue from 'vue';
 import VueMaterial from 'vue-material';
+import firebase from 'firebase';
+import VueFire from 'vuefire';
 import 'vue-material/dist/vue-material.min.css';
 import 'vue-material/dist/theme/default.css';
 
 import App from './Fammer.vue';
 
 Vue.use(VueMaterial);
+Vue.use(VueFire);
 
 // eslint-disable-next-line no-new
 new Vue({
