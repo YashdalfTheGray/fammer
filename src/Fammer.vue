9c52f3a09878b310c64f53f0f793f5143e6820fa
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2018-02-11 10:47:26 -0800

    added the search bar to the main page

--- src/Fammer.vue
+++ src/Fammer.vue
@@ -2,9 +2,13 @@
     <md-app id="fammer" md-waterfall md-mode="fixed" class="fammer-app-shell">
         <md-app-toolbar class="md-primary">
             <h3 class="md-title" style="flex: 1">{{ appName }}</h3>
+            <md-button class="md-icon-button" @click="searchVisible = !searchVisible">
+                <md-icon>search</md-icon>
+            </md-button>
             <md-button>Log In</md-button>
         </md-app-toolbar>
         <md-app-content class="md-scrollbar">
+            <search-bar v-show="searchVisible" @query-changed="searchQueryChanged"></search-bar>
             <restaurant-grid></restaurant-grid>
         </md-app-content>
     </md-app>
@@ -12,14 +16,25 @@
 
 <script>
 import RestaurantGrid from './components/RestaurantGrid.vue';
+import SearchBar from './components/SearchBar.vue';
 
 export default {
     name: 'fammer',
-    data: () => ({
-        appName: 'Fammer'
-    }),
+    data() {
+        return {
+            appName: 'Fammer',
+            query: '',
+            searchVisible: false
+        };
+    },
     components: {
-        RestaurantGrid
+        RestaurantGrid,
+        SearchBar
+    },
+    methods: {
+        searchQueryChanged(query) {
+            this.query = query;
+        }
     }
 };
 </script>
